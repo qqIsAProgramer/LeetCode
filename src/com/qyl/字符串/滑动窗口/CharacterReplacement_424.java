d88fe7cb950055127cc96c243d6a8277e217e2c7
QiuYiliang <1162922438@qq.com>
2021-02-02 23:04:00 +0800

    2.2字符串

---
+++ src/com/qyl/字符串/滑动窗口/CharacterReplacement_424.java
@@ -0,0 +1,33 @@
+package com.qyl.字符串.滑动窗口;
+
+/**
+ * @Author: qyl
+ * @Date: 2021/2/2 16:36
+ */
+public class CharacterReplacement_424 {
+
+    /**
+     * 给你一个仅由大写英文字母组成的字符串，你可以将任意位置上的字符替换成另外的字符，总共可最多替换 k 次
+     * 在执行上述操作后，找到包含重复字母的最长子串的长度
+     * -----------------------------------------------------------------------------------
+     * 解法：
+     * 滑动窗口（非常多细节）
+     * @param s
+     * @param k
+     * @return
+     */
+    public int characterReplacement(String s, int k) {
+        int len = s.length();
+        if (len < 2) {
+            return len;
+        }
+        char[] charArray = s.toCharArray();
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        int maxCount = 0;
+
+        return res;
+    }
+}
