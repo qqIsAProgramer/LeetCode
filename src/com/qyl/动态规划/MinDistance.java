f90e15b39a6f6c6833c5d1fc28676fc1ede1959e
QiuYiliang <1162922438@qq.com>
2021-04-04 11:40:49 +0800

    4.3编辑距离

---
+++ src/com/qyl/动态规划/MinDistance.java
@@ -0,0 +1,42 @@
+package com.qyl.动态规划;
+
+/**
+ * @Author: qyl
+ * @Date: 2021/4/4 11:06
+ */
+public class MinDistance {
+
+    /**
+     * 编辑距离（动态规划超经典）
+     * id: 72
+     * level: hard
+     * 解法：
+     * 看 powcai 大神题解
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
